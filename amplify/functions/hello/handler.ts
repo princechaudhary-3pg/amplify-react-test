5ba1e0f116fbdad41b35c5695124bd8ab2305c93
princechaudhary-3pg <prince.chaudhary@3pillarglobal.com>
2026-02-08 19:34:03 +0530

    Add Environment Variables

--- amplify/functions/hello/handler.ts
+++ amplify/functions/hello/handler.ts
@@ -1,12 +1,11 @@
 import type { Schema } from "../../data/resource";
-import { TABLE_NAME } from "./resource";
-// import { env } from "$amplify/env/sayHello";
+import { env } from "$amplify/env/sayHello";
 
 type handlerType = Schema["sayHello"]["functionHandler"];
-const env = { AWS_REGION: 'us-east-5', TABLE_NAME }
+
 export const handler: handlerType = async (event) => {
-  const region = env.AWS_REGION
+  const region = env.REGION
   const tableName = env.TABLE_NAME
   const { name } = event.arguments;
-  return `Hello ${name},  my first function is ready! table name: ${tableName}, region: ${region}`;
+  return `Hello ${name},  my first function is ready! tableName: ${tableName}, region: ${region}`;
 };
