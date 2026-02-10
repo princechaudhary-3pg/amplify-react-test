888fd683b149a127caa6fae8b5ee15e3ac668332
princerathi1989 <princerathi1989@gmail.com>
2026-02-10 17:20:30 +0530

    Add Storage

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,23 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { sayHello } from './functions/hello/resource';
+import { imagesStorage } from './storage/resource';
+import { generateThumb } from './functions/resize/resource';
+import { EventType } from 'aws-cdk-lib/aws-s3';
+import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
-  sayHello
+  sayHello,
+  imagesStorage,
+  generateThumb
 });
+
+backend.imagesStorage.resources.bucket.addEventNotification(
+  EventType.OBJECT_CREATED_PUT,
+  new LambdaDestination(backend.generateThumb.resources.lambda),
+  {
+    prefix: 'originals/'
+  }
+)
---
+++ amplify/functions/resize/resource.ts
@@ -0,0 +1,10 @@
+import { defineFunction } from "@aws-amplify/backend";
+
+export const generateThumb = defineFunction({
+  name: "resize-image",
+  entry: "./handler.ts",
+  bundling: {
+    minify: false
+  },
+  timeoutSeconds: 60
+});
